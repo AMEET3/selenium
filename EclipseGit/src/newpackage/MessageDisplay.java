64d5eb9792d2b01381dc925cb7375d815fa3f742
ameet.gondali@thomsonreuters.com <ameet.gondali@thomsonreuters.com>
2018-06-05 15:55:20 +0530

    Hare krishna

---
+++ EclipseGit/src/newpackage/MessageDisplay.java
@@ -0,0 +1,16 @@
+package newpackage;
+
+public class MessageDisplay {
+
+	public MessageDisplay() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This Code is working fine Krishna");
+
+	}
+
+}
